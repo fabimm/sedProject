ffbce5e1e09337c6b2b711e8756297907e507820
Allen Perez <00053812@uca.edu.sv>
2018-11-20 17:20:31 -0600

    Agregados algunas  clases para POSTGRES

--- SED/site/config.php
+++ SED/site/config.php
@@ -3,10 +3,7 @@
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'sed');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   if ($db->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE); 
 //    $hostname = 'localhost:3306';
 //    $username = 'root';
 //    $password = 'root';
@@ -14,4 +11,11 @@
 
 //    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 //    $db=pg_connect("host=$hostname dbname=$database user=$username password=$password");
+//    $dbconn = pg_connect("dbname=publisher") or die("Could not connect");
+//    $stat = pg_connection_status($dbconn);
+//    if ($stat === PGSQL_CONNECTION_OK) {
+//        echo 'Connection status ok';
+//    } else {
+//        echo 'Connection status bad';
+//    }
 ?>
